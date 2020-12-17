abdbce0b4926f11c68ce70e9283ac512739033cb
Ved-arch <75379440+Ved-arch@users.noreply.github.com>
2020-12-17 20:43:13 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,43 @@
+var fixedRect,movingRect;
+
+var gameObject1,gameObject2,gameObject3,gameObject4;
+function setup(){
+  createCanvas(1200,800);
+  fixedRect = createSprite(600,200,50,80);
+  fixedRect.shapeColor = "green";
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  gameObject1 = createSprite(100,100,50,50);
+  gameObject1.shapeColor = "red";
+  gameObject2 = createSprite(200,100,50,50);
+  gameObject2.shapeColor = "red"
+  gameObject3 = createSprite(300,100,50,50);
+  gameObject3.shapeColor = "red";
+  gameObject4 = createSprite(400,100,50,50);
+  gameObject4.shapeColor = "red";
+}
+function draw(){
+  background(0);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  if(isTouching(movingRect,gameObject2)){
+    movingRect.shapeColor = "blue";
+  gameObject2.shapeColor = "blue";
+  }
+  else{
+    movingRect.shapeColor = "green";
+    gameObject2.shapeColor = "red";
+  }
+  drawSprites();
+}
+  function isTouching(object1,object2){
+    if(object1.x-object2.x<object2.width / 2 + object1.width / 2 && 
+      object1.x-object1.x< object2.width/ 2 + object1.width/ 2 &&
+      object1.y-object2.y<object2.width/ 2 + object1.width/ 2 && 
+      object2.y-object1.y<object2.width/ 2 +object1.width/ 2){
+return true;
+    }
+    else{
+      return false;
+    }
+  }
